1f97a88edfc887a60abf2bdd3d22e963a4c09fa6
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2025-08-17 10:08:09 +0200

    Project Initialization

---
+++ department-service-urf/src/main/java/com/dgmf/department_service_urf/DepartmentServiceUrfApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.department_service_urf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceUrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceUrfApplication.class, args);
+	}
+
+}
---
+++ department-service-urf/src/test/java/com/dgmf/department_service_urf/DepartmentServiceUrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.department_service_urf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceUrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
